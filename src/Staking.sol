ffc2bc23f5861cd5b16bd81ed1a3e14290673287
Kshitij <srivastavakshitijprofessional@gmail.com>
2026-07-11 08:40:55 +0000

    feat: add withdraw

--- src/Staking.sol
+++ src/Staking.sol
@@ -22,25 +22,25 @@ contract Staking {
     uint256 public immutable PRECISION_FACTOR;
 
     mapping(address => UserInfo) private userInfo;
+
     struct UserInfo {
         uint256 amount;
         uint256 rewardPerSharePaid;
     }
+
     struct TokenConfig {
         address stakedToken;
         address rewardToken;
         uint8 rewardTokenDecimals;
     }
+
     struct RewardConfig {
         uint256 rewardPerBlock;
         uint256 rewardStartBlock;
         uint256 rewardEndBlock;
     }
 
-    constructor(
-        address initialOwner, 
-        TokenConfig memory tokenConfig, 
-        RewardConfig memory rewardConfig)
+    constructor(address initialOwner, TokenConfig memory tokenConfig, RewardConfig memory rewardConfig)
         Ownable(initialOwner)
     {
         stakedToken = IERC20(tokenConfig.stakedToken);
@@ -58,74 +58,88 @@ contract Staking {
         lastRewardBlock = rewardStartBlock;
     }
 
-
     function deposit() external {
-         UserInfo storage user = userInfo[msg.sender];
+        UserInfo storage user = userInfo[msg.sender];
         _updatePoolRewards();
-        _payPendingRewards(user,msg.sender);
-        _increaseStake(user,msg.sender,amount);
+        _payPendingRewards(user, msg.sender);
+        _increaseStake(user, msg.sender, amount);
 
-         user.rewardPerSharePaid = (user.amount*accumulatedRewardPerShare)/PRECISION_FACTOR;
+        user.rewardPerSharePaid = (user.amount * accumulatedRewardPerShare) / PRECISION_FACTOR;
     }
 
     function _updatePoolRewards() internal {
-         if(block.number<=lastRewardBlock){
-            return;
-         }
-         if(totalShareAmount==0){
-            lastRewardBlock = block.number;
-            return;
-         }                                                                    
-         uint256 reward = rewardPerBlock*_getRewardBlockCount(lastRewardBlock,block.number);
-         accumulatedRewardPerShare+=reward*PRECISION_FACTOR/totalShareAmount;
-         lastRewardBlock = block.number;
-    }
-     function _getRewardBlockCount(uint256 from,uint256 to) returns(uint256){
-         if(to<=rewardEndBlock){
-            return to-from;
-         }
-         else if(from>=rewardEndBlock){
-             return 0;
-         }else{
-            rewardEndBlock-from;
-         }
-    }
-
-    function _payPendingRewards(UserInfo storage user,address account) internal{
-          if(user.amount==0){
-             return;
-          }
-          uint256 pendingReward = (user.amount*accumulatedRewardPerShare)/PRECISION_FACTOR - user.rewardPerSharePaid;
-
-          if(pendingReward>0){
-             rewardToken.safeTransfer(user,pendingReward);
-          }
-    }
-
-    function _increaseStake(UserInfo storage user,address account,uint256 amount) internal{
-        if(amount==0){
+        if (block.number <= lastRewardBlock) {
             return;
         }
-        user.amount+=amount;
-        totalShareAmount+=amount;
-        stakedToken.safeTransferFrom(account,address(this),amount);
+        if (totalShareAmount == 0) {
+            lastRewardBlock = block.number;
+            return;
+        }
+        uint256 reward = rewardPerBlock * _getRewardBlockCount(lastRewardBlock, block.number);
+        accumulatedRewardPerShare += reward * PRECISION_FACTOR / totalShareAmount;
+        lastRewardBlock = block.number;
     }
 
-     function withdraw() external {
-         UserInfo storage user = userInfo[msg.sender];
+    function _getRewardBlockCount(uint256 from, uint256 to) returns (uint256) {
+        if (to <= rewardEndBlock) {
+            return to - from;
+        } else if (from >= rewardEndBlock) {
+            return 0;
+        } else {
+            rewardEndBlock - from;
+        }
+    }
+
+    function _payPendingRewards(UserInfo storage user, address account) internal {
+        if (user.amount == 0) {
+            return;
+        }
+        uint256 pendingReward = (user.amount * accumulatedRewardPerShare) / PRECISION_FACTOR - user.rewardPerSharePaid;
+
+        if (pendingReward > 0) {
+            rewardToken.safeTransfer(user, pendingReward);
+        }
+    }
+
+    function _increaseStake(UserInfo storage user, address account, uint256 amount) internal {
+        if (amount == 0) {
+            return;
+        }
+        user.amount += amount;
+        totalShareAmount += amount;
+        stakedToken.safeTransferFrom(account, address(this), amount);
+    }
+
+    function withdraw() external {
+        UserInfo storage user = userInfo[msg.sender];
         _updatePoolRewards();
         _payPendingRewards();
-        _decreaseStake(user,msg.sender,amount);
+        _decreaseStake(user, msg.sender, amount);
+    }
+
+    function _decreaseStake(UserInfo storage user, address account, uint256 amount) internal {
+        if (amount > user.amount) {
+            return;
+        }
+        user.amount -= amount;
+        totalShareAmount -= amount;
+        stakedToken.safeTransfer(account, amount);
     }
 
     function stopRewards() external onlyOwner {
         rewardEndBlock = block.number;
     }
 
-      function updateRewardPerBlock(uint256 newRewardPerBlock) external onlyOwner {
-        require(rewardStartBlock<newRewardPerBlock, "Staking has not started");
+    function updateRewardPerBlock(uint256 newRewardPerBlock) external onlyOwner {
+        require(rewardStartBlock < newRewardPerBlock, "Staking has not started");
         rewardPerBlock = newRewardPerBlock;
     }
 
+    function updateRewardStartBlock(uint256 newRewardStartBlock) external onlyOwner {
+        rewardStartBlock = newRewardStartBlock;
+    }
 
+    function updateRewardEndBlock(uint256 newRewardEndBlock) external onlyOwner {
+        rewardEndBlock = newRewardEndBlock;
+    }
 }
